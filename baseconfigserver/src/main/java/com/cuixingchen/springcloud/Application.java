19df8e8f591e75a1d33636845730f9fa122b75cf
cuipengfei <cuipf1007>
2017-04-21 15:52:38 +0800

    spring cloud configserver配置

--- baseconfigserver/src/main/java/com/cuixingchen/springcloud/Application.java
+++ baseconfigserver/src/main/java/com/cuixingchen/springcloud/Application.java
@@ -2,11 +2,13 @@ package com.cuixingchen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by cuipengfei on 16-10-18.
  */
 @SpringBootApplication
+@EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
