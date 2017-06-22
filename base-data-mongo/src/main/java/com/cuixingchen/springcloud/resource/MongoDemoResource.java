5433a74405759905f31936796d4e1013d53ee7e9
cuipengfei <cuipf1007>
2017-06-22 17:32:01 +0800

    重命名

--- base-data-mongo/src/main/java/com/cuixingchen/springcloud/resource/MongoDemoResource.java
+++ base-data-mongo/src/main/java/com/cuixingchen/springcloud/resource/MongoDemoResource.java
@@ -1,6 +1,5 @@
 package com.cuixingchen.springcloud.resource;
 
-import com.mongodb.MongoClientOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
