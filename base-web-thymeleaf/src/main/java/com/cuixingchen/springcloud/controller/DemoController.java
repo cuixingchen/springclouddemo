e9d54669002732f82bf88780181b5da0fea9af98
cuipengfei <cuipf1007>
2017-06-23 17:31:09 +0800

    整合feign

--- base-web-thymeleaf/src/main/java/com/cuixingchen/springcloud/controller/DemoController.java
+++ base-web-thymeleaf/src/main/java/com/cuixingchen/springcloud/controller/DemoController.java
@@ -1,11 +1,17 @@
 package com.cuixingchen.springcloud.controller;
 
+import com.cuixingchen.springcloud.feign.UserFeign;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by cuipengfei on 17-6-15.
@@ -16,9 +22,17 @@ public class DemoController {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoController.class);
 
+    @Autowired
+    UserFeign userFeign;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     public String get(Model model) {
-        model.addAttribute("name","哈哈");
+        try {
+            ResponseEntity<List<Map>> result = userFeign.get("");
+            model.addAttribute("name","哈哈");
+        }catch (Exception e){
+            logger.error("demo get异常",e);
+        }
         return "demo/demo_list";
     }
 }
